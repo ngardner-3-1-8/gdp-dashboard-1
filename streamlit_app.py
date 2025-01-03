30c737735fe817078ed3590c19c09d336a3c0017
ngardner-3-1-8 <ngardner318@gmail.com>
2025-01-03 08:48:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1230,11 +1230,15 @@ def calculate_ev():
 
 st.title("NFL Survivor Optimization")
 st.subheader("The second best Circa Survivor Contest optimizer")
+contest_options = [
+    "Circa", "DraftKings"
+]
 st.write("Alright, clowns. This site is built to help you optimize your picks for the Circa Survivor contest (Eventually other contests). :red[This tool is just for informational use. It does not take into account injuries or certain other factors. Do not use this tool as your only source of information.] Simply input which week you're in, your team rankings, constraints, etc... and the algorithm will do the rest. For a full season, Running the algorith will take up to 5 hours or more. Good luck!")
 st.write('')
 st.write('')
 st.subheader('Select Contest')
 st.write('Choose the contest you are using this algorithm for: Circa (Advanced) or Draftkings (Traditional and Pathetic)')
+selected_content = st.selectbox('Choose Contest:', options = contest_options)
 st.write('')
 st.write('')
 st.write('')

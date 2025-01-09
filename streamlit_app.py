c742b37b4022c193edb659b1c19d468980698db4
ngardner-3-1-8 <ngardner318@gmail.com>
2025-01-08 18:20:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1874,7 +1874,7 @@ def get_survivor_picks_based_on_ev():
 
             # Initialize picks_df
             picks_df = pd.DataFrame(columns=df.columns)
-			picks_rows_2 = []
+picks_rows_2 = []
 
             for i in range(len(df)):
                 if picks[i].solution_value() > 0:

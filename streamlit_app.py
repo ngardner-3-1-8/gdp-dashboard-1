ed3f55560cab0d2ddc834407e61dd38a667a193e
ngardner-3-1-8 <ngardner318@gmail.com>
2025-08-09 11:10:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -560,7 +560,7 @@ def collect_schedule_travel_ranking_data(pd):
         options = uc.ChromeOptions()
         
         # Using the new headless mode is less detectable than the old one
-        options.add_argument("--headless=new") 
+        #options.add_argument("--headless=new") 
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")

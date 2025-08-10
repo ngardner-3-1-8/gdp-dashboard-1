c76c2b2ad5a00ce8b297d7fc1a4fc1508e62dcd6
ngardner-3-1-8 <ngardner318@gmail.com>
2025-08-10 11:07:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3026,7 +3026,7 @@ def get_survivor_picks_based_on_ev():
     
             # Objective: maximize the sum of Adjusted Current Difference of each game picked
 #            solver.Maximize(solver.Sum([picks[i] * (df.loc[i, 'Home Team EV'] if df.loc[i, 'Adjusted Current Winner'] == df.loc[i, 'Home Team'] else df.loc[i, 'Away Team EV']) for i in range(len(df))]))
-			solver.Maximize(solver.Sum([picks[i] * (df.loc[i, 'Home Team EV'] if (df.loc[i, 'Adjusted Current Winner'] == df.loc[i, 'Home Team'] and df.loc[i, 'Favorite'] == df.loc[i, 'Home Team']) else (df.loc[i, 'Away Team EV'] if (df.loc[i, 'Adjusted Current Winner'] == df.loc[i, 'Away Team'] and df.loc[i, 'Favorite'] == df.loc[i, 'Away Team']) else 0)) for i in range(len(df))]))
+            solver.Maximize(solver.Sum([picks[i] * (df.loc[i, 'Home Team EV'] if (df.loc[i, 'Adjusted Current Winner'] == df.loc[i, 'Home Team'] and df.loc[i, 'Favorite'] == df.loc[i, 'Home Team']) else (df.loc[i, 'Away Team EV'] if (df.loc[i, 'Adjusted Current Winner'] == df.loc[i, 'Away Team'] and df.loc[i, 'Favorite'] == df.loc[i, 'Away Team']) else 0)) for i in range(len(df))]))
     
             # Solve the problem and print the solution
             status = solver.Solve()

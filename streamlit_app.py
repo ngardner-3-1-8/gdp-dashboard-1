319dacde9dc9662a66877f66845b904f91bac8b8
ngardner-3-1-8 <ngardner318@gmail.com>
2025-09-06 01:30:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -868,7 +868,10 @@ def collect_schedule_travel_ranking_data(pd):
                 csv_df.loc[index, 'Away Team Moneyline'] = row['Internal Away Team Moneyline']
             if pd.isna(row['Home Team Moneyline']):
                 csv_df.loc[index, 'Home Team Moneyline'] = row['Internal Home Team Moneyline']
-            
+			if pd.isna(row['Home Team Sportsbook Spread']) or row['Home Team Sportsbook Spread'] is None:
+				csv_df.loc[index, 'Home Team Sportsbook Spread'] = ['Internal Home Team Spread']
+            if pd.isna(row['Away Team Sportsbook Spread']) or row['Away Team Sportsbook Spread'] is None:
+				csv_df.loc[index, 'Away Team Sportsbook Spread'] = row['Internal Away Team Spread']		            
             # Override Favorite/Underdog if not set by DraftKings (i.e., still NaN)
             if pd.isna(row['Favorite']) or row['Favorite'] is None:
                 # Determine Favorite and Underdog based on internal ranks
@@ -947,14 +950,6 @@ def collect_schedule_travel_ranking_data(pd):
                         'Internal Away Team Fair Odds', 'Internal Home Team Fair Odds']:
                 if not pd.isna(csv_df.loc[index, col]): # Only round if not NaN
                     csv_df.loc[index, col] = round(csv_df.loc[index, col], 4)
-            if pd.isna(row['Away Team Moneyline']) or row['Away Team Moneyline'] is None:
-                overridden_games_df.loc[index, 'Away Team Moneyline'] = row['Internal Away Team Moneyline']
-            if pd.isna(row['Home Team Moneyline']) or row['Home Team Moneyline'] is None:
-                overridden_games_df.loc[index, 'Home Team Moneyline'] = row['Internal Home Team Moneyline']
-            if pd.isna(row['Home Team Sportsbook Spread']) or row['Home Team Sportsbook Spread'] is None:
-                overridden_games_df.loc[index, 'Home Team Sportsbook Spread'] = row['Away Team Adjusted Current Rank'] - row['Home Team Adjusted Current Rank']
-            if pd.isna(row['Away Team Sportsbook Spread']) or row['Away Team Sportsbook Spread'] is None:
-                overridden_games_df.loc[index, 'Away Team Sportsbook Spread'] = row['Home Team Adjusted Current Rank'] - row['Away Team Adjusted Current Rank']
     
         main_df_with_odds_df = csv_df
         return main_df_with_odds_df

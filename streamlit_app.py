e2de275f43af37893693cfac5a7fcaef45d4fe1d
ngardner-3-1-8 <ngardner318@gmail.com>
2025-09-05 20:18:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -841,7 +841,7 @@ def collect_schedule_travel_ranking_data(pd):
         override_condition = pd.isna(csv_df['Away Team Moneyline']) | pd.isna(csv_df['Home Team Moneyline'])
         csv_df['No Live Odds Available - Internal Rankings Used for Moneyline Calculation'] = np.where(override_condition, 'True', 'False')
         overridden_games_df = csv_df[override_condition]
-        overridden_games_df = csv_df[override_condition][['Home Team', 'Away Team', 'Actual Stadium', 'Date', 'Home Team Moneyline', 'Away Team Moneyline', 'Internal Home Team Moneyline', 'Internal Away Team Moneyline']].copy()
+        overridden_games_df = csv_df[override_condition][['Home Team', 'Away Team', 'Actual Stadium', 'Date', 'Home Team Moneyline', 'Away Team Moneyline', 'Internal Home Team Moneyline', 'Internal Away Team Moneyline', 'Home Team Sportsbook Spread', 'Away Team Sportsbook Spread']].copy()
 
         for index, row in overridden_games_df.iterrows():
             # Override Moneyline if DraftKings data was missing (still NaN)
@@ -849,10 +849,10 @@ def collect_schedule_travel_ranking_data(pd):
                 overridden_games_df.loc[index, 'Away Team Moneyline'] = row['Internal Away Team Moneyline']
             if pd.isna(row['Home Team Moneyline']):
                 overridden_games_df.loc[index, 'Home Team Moneyline'] = row['Internal Home Team Moneyline']
-#            if pd.isna(row['Home Team Sportsbook Spread']):
-#                overridden_games_df.loc[index, 'Home Team Sportsbook Spread'] = row['Away Team Adjusted Current Rank'] - row['Home Team Adjusted Current Rank']
-#            if pd.isna(row['Away Team Sportsbook Spread']):
-#                overridden_games_df.loc[index, 'Away Team Sportsbook Spread'] = row['Home Team Adjusted Current Rank'] - row['Away Team Adjusted Current Rank']
+            if pd.isna(row['Home Team Sportsbook Spread']):
+                overridden_games_df.loc[index, 'Home Team Sportsbook Spread'] = row['Away Team Adjusted Current Rank'] - row['Home Team Adjusted Current Rank']
+            if pd.isna(row['Away Team Sportsbook Spread']):
+                overridden_games_df.loc[index, 'Away Team Sportsbook Spread'] = row['Home Team Adjusted Current Rank'] - row['Away Team Adjusted Current Rank']
         st.subheader('Games with Unavailable Live Odds')
         st.write('This dataframe contains the games where live odds from the Live Odds API were unavailable. This will likely happen for lookahead lines and future weeks')
         st.write(overridden_games_df)

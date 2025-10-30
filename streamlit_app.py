c92004acd1bc56d451142c0f006b10b5817cc368
ngardner-3-1-8 <ngardner318@gmail.com>
2025-10-30 16:57:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4204,7 +4204,7 @@ else:
             args=('subcontest',),
             help=subcontest_help_text
         )
-
+        subcontest = st.session_state.current_config['subcontest']
         if subcontest != "Week 9 Bloody Survivor ($100 Entry)":
             st.multiselect(
 	            "Which weeks do you need to select two teams?:",

2732ab460fe036c8689e1bc362000536fd6be39b
ngardner-3-1-8 <ngardner318@gmail.com>
2025-10-31 09:49:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4048,23 +4048,23 @@ else:
         # Dynamically set ending week based on default contest
         if st.session_state.current_config['selected_contest'] != 'Circa':
             st.session_state.current_config['ending_week'] = 19
-	subcontest = st.session_state.current_config['subcontest']
-	if st.session_state.current_config['subcontest'] == "The Big Splash ($150 Entry)":
-		st.session_state.current_config['weeks_two_picks'] = [11, 12, 13, 14, 15, 16, 17, 18]
-	elif st.session_state.current_config['subcontest'] == "4 for 4 ($50 Entry)":
-		st.session_state.current_config['weeks_two_picks'] = [11, 12, 13, 14, 15, 16, 17, 18]
-	elif st.session_state.current_config['subcontest'] == "Free RotoWire (Free Entry)":
-		st.session_state.current_config['weeks_two_picks'] = []
-	elif st.session_state.current_config['subcontest'] == "For the Fans ($40 Entry)":
-		st.session_state.current_config['weeks_two_picks'] = [14, 15, 16, 17, 18]
-	elif st.session_state.current_config['subcontest'] == "Walker's Ultimate Survivor ($25 Entry)":
-		st.session_state.current_config['weeks_two_picks'] = [6, 12, 13, 14, 15, 16, 17, 18]
-	elif st.session_state.current_config['subcontest'] == "Ship It Nation ($25 Entry)":
-		st.session_state.current_config['weeks_two_picks'] = [12, 13, 14, 15, 16, 17, 18]
-	elif st.session_state.current_config['subcontest'] == "High Roller ($1000 Entry)":
-		st.session_state.current_config['weeks_two_picks'] = []
-	elif st.session_state.current_config['subcontest'] == "Week 9 Bloody Survivor ($100 Entry)":
-		st.session_state.current_config['weeks_three_picks'] = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+    subcontest = st.session_state.current_config['subcontest']
+    if st.session_state.current_config['subcontest'] == "The Big Splash ($150 Entry)":
+        st.session_state.current_config['weeks_two_picks'] = [11, 12, 13, 14, 15, 16, 17, 18]
+    elif st.session_state.current_config['subcontest'] == "4 for 4 ($50 Entry)":
+        st.session_state.current_config['weeks_two_picks'] = [11, 12, 13, 14, 15, 16, 17, 18]
+    elif st.session_state.current_config['subcontest'] == "Free RotoWire (Free Entry)":
+        st.session_state.current_config['weeks_two_picks'] = []
+    elif st.session_state.current_config['subcontest'] == "For the Fans ($40 Entry)":
+        st.session_state.current_config['weeks_two_picks'] = [14, 15, 16, 17, 18]
+    elif st.session_state.current_config['subcontest'] == "Walker's Ultimate Survivor ($25 Entry)":
+        st.session_state.current_config['weeks_two_picks'] = [6, 12, 13, 14, 15, 16, 17, 18]
+    elif st.session_state.current_config['subcontest'] == "Ship It Nation ($25 Entry)":
+        st.session_state.current_config['weeks_two_picks'] = [12, 13, 14, 15, 16, 17, 18]
+    elif st.session_state.current_config['subcontest'] == "High Roller ($1000 Entry)":
+        st.session_state.current_config['weeks_two_picks'] = []
+    elif st.session_state.current_config['subcontest'] == "Week 9 Bloody Survivor ($100 Entry)":
+        st.session_state.current_config['weeks_three_picks'] = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
 
     # --- Initialize other session state variables ---
     if 'config_status' not in st.session_state:
